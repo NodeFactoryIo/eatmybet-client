38349a20ea8cc632ccb44ae51375ddbca0cf5174
morrigan <belma.dzi@gmail.com>
2018-06-14 19:05:14 +0200

    Remove truffle contract

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,5 +1,5 @@
 import { all, takeLatest, call, put, fork } from 'redux-saga/effects';
-import truffleContract from 'truffle-contract';
+import Web3 from 'web3';
 import actions from './actions';
 import { fetchContracts, fetchGames } from "./api";
 
@@ -8,11 +8,15 @@ function* contract() {
     try {
       const data = yield call(fetchContracts);
 
-      const contract = truffleContract(data.EatMyBetContract);
-      contract.setProvider(window.web3.currentProvider);
-      const eatMyBetContract = yield call(contract.deployed);
+      const web3 = new Web3(Web3.givenProvider);
+      web3.eth.getAccounts((error, accounts) => {
+        web3.eth.defaultAccount = accounts[0];
+      });
+      const contract = data.EatMyBetContract;
+      const eatMyBetContract = new web3.eth.Contract(contract.abi, contract.networks[666].address);
 
       yield put({type: actions.FETCH_CONTRACTS_SUCCESS, data: eatMyBetContract });
+      yield put({type: actions.INIT_WEB3_SUCCESS, data: web3 });
     } catch (e) {
       yield put({type: actions.ERROR, message: e.message});
     }
