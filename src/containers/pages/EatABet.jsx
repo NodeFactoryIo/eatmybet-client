87cb961c352cd4fc4b82ee68c809105ffc0d8c99
morrigan <belma.dzi@gmail.com>
2018-06-15 22:54:03 +0200

    Read bet pools for bet eating

--- src/containers/pages/EatABet.jsx
+++ src/containers/pages/EatABet.jsx
@@ -1,51 +1,16 @@
 import React from 'react';
-import { connect } from "react-redux";
-import PropTypes from 'prop-types';
 
 import Header from '../Header';
 import EatABetList from '../EatABetList';
 
 
-class EatABetPage extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      betPools: [],
-    }
-  }
-
-  componentDidMount() {
-    //const { contract } = this.props;
-    //contract.getBetPoolSize.then((size) => {
-    // })
-    // Missing getBEtPoolSize function
-    // const betPools = [];
-    // for (let i = 0; i < 1; i++) {
-    //   betPools.push(contract.betPools(i));
-    // }
-    // console.log(betPools);
-  }
-
+export default class EatABetPage extends React.Component {
   render() {
-    console.log(this.context);
     return (
-      <div class="eat-a-bet-page">
+      <div className="eat-a-bet-page">
         <Header />
         <EatABetList />
       </div>
     );
   }
 }
-
-EatABetPage.contextTypes = {
-  web3: PropTypes.object
-};
-
-const mapStateToProps = state => ({
-  contract: state.contract,
-});
-
-export default connect(
-  mapStateToProps,
-)(EatABetPage);
