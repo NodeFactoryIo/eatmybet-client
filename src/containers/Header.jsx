394619d2ab0dac6f7426a40b2c8da6c41f5f6d2a
Željko Dragosavac <zeljko.dragosavac@m2bit.com>
2018-06-18 16:21:03 +0200

    Added exact to place-a-bet header link

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -9,7 +9,7 @@ class Header extends React.Component {
         <div className="content">
           <a className="logo" href="/"><img src="./images/logo/logo-short.png" alt="EatMyBet" /></a>
 
-          <NavLink activeClassName="active" className="place" to="/">Place a bet</NavLink>
+          <NavLink activeClassName="active" className="place" to="/" exact>Place a bet</NavLink>
           <NavLink activeClassName="active" className="eat" to="/eat-a-bet">Eat a bet</NavLink>
           <NavLink activeClassName="active" className="my-bets" to="/my-bets">My bets</NavLink>
         </div>
