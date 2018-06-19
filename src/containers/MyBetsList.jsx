6214ee31f2717b65ea87d0e50a156b17a6c22737
Željko Dragosavac <zeljko.dragosavac@m2bit.com>
2018-06-19 12:11:53 +0200

    My bets list - adding custom actions and styling

--- src/containers/MyBetsList.jsx
+++ src/containers/MyBetsList.jsx
@@ -47,28 +47,54 @@ class MyBetsList extends React.Component {
   }
 
   render() {
+
     const { games, web3 } = this.props;
     const { bets, betsLoaded, poolsLoaded } = this.state;
+
+    const dateTimeNowWithGameEndOffset = moment.utc().add({ hours: 2});
 
     console.log(bets);
 
     if (!bets || games.length === 0) {
       return (
-        <div className="place-a-bet-wrap">'Loading...'</div>
+        <div className="my-bets-wrap">'Loading...'</div>
       )
     }
 
     if (_.isEmpty(bets) && betsLoaded && poolsLoaded) {
       return (
-        <div className="place-a-bet-wrap"><h2>You have no bets, but you <a href="/">create a new one</a> or <a href="/eat-a-bet">eat an exiting one</a></h2></div>
+        <div className="my-bets-wrap"><h2>You have no bets, but you <a href="/">create a new one</a> or <a href="/eat-a-bet">eat an exiting one</a></h2></div>
       )
     }
 
     return (
-      <div className="place-a-bet-wrap">
+      <div className="my-bets-wrap">
         {Object.values(bets).map(function(betObject, index){
           const bet = betObject.returnValues;
           const game = _.filter(games, { gameId: bet.gameId})[0];
+
+          let actionInfo = 'cancel';
+          
+          let isBetTaken = true;
+          let waitingForGameOutcome = true; // moment.utc(game.dateTime) < dateTimeNowWithGameEndOffset
+          let betHasResult = false;
+          let isUserWinner = false;
+
+          if (isBetTaken) { 
+            actionInfo = 'none';
+          }
+
+          if (isBetTaken && waitingForGameOutcome) { 
+            actionInfo = 'waiting';
+          }
+
+          if (betHasResult && !isUserWinner) {
+            actionInfo = 'none';
+          }
+
+          if (betHasResult && isUserWinner) {
+            actionInfo = 'collect';
+          } 
 
           return (
           <div className="place-a-bet game" key={index}>
@@ -80,20 +106,20 @@ class MyBetsList extends React.Component {
                     <span className="time">{ moment.utc(game.dateTime).local().format('HH:mm') }</span>
                   </div>
                   <div className="home col-4-12">
-                    <button className="action home" onClick={() => this.onGameClick(game.gameId, 1)}>
+                    <button disabled className={"home " + (bet.bet === "1" ? 'active' : 'inactive')}>
                       <div className="flag" style={{ backgroundImage: 'url(/images/flags/' + game.homeTeamNameShort + '.png'  }} />
                       {game.homeTeamNameShort}
                     </button>
                   </div>
 
                   <div className="seperator col-2-12">
-                    <button className="action draw" onClick={() => this.onGameClick(game.gameId, 2)}>
+                  <button disabled className={"draw " + (bet.bet === "2" ? 'active' : 'inactive')}>
                       X
                     </button>
                   </div>
 
                   <div className="away col-4-12">
-                    <button className="action away" onClick={() => this.onGameClick(game.gameId, 3)}>
+                  <button disabled className={"away " + (bet.bet === "3" ? 'active' : 'inactive')}>
                       <div className="flag" style={{ backgroundImage: 'url(/images/flags/' + game.awayTeamNameShort + '.png'  }} />
                       {game.awayTeamNameShort}
                     </button>
@@ -104,18 +130,31 @@ class MyBetsList extends React.Component {
               <div className="action col-1-4">
                 <div className={`grid grid-pad-small info active}`}>
                     <div className="col-6-12">
-                      <span>Odd</span>
+                      <span className="label">Odd</span>
                       <span className="value">{bet.coef / 100}</span>
                     </div>
                     <div className="col-6-12">
-                      <span>Amount</span>
+                      <span className="label">Amount</span>
                       <span className="value">{parseFloat(web3.utils.fromWei(bet.amount)).toFixed(3)}</span>
                     </div>
                   </div>
               </div>
 
               <div className="action col-1-6">
-                <button className="place" onClick={() => this.getResults(bet.betPoolId)}>View results</button>
+                {{
+                  'cancel': (
+                    <button className="cancel" onClick={() => this.getResults(bet.betPoolId)}>Cancel</button>
+                  ),
+                  'waiting': (
+                    <span className="button waiting">...</span>
+                  ),
+                  'collect': (
+                    <button className="collect" onClick={() => this.getResults(bet.betPoolId)}>Cancel</button>
+                  ),
+                  default: (
+                    <span></span>
+                  )
+                }[actionInfo]}
               </div>
             </div>
           </div>
